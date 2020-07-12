a1eb8ac0fb81f51db64e60f8a2ee1f74726137cf
leonardons93 <43036983+leonardons93@users.noreply.github.com>
2020-07-11 22:28:42 -0300

    alterações leo

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
+import React ,{Component} from 'react';
+import { StyleSheet ,Text, View} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import { ActivityIndicator } from "react-native";
+import { Header, Content, List, ListItem, Icon, Title} from 'native-base';
 
-export default function App() {
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async UNSAFE_componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+     
+    });
+    this.setState({ loading: false });
+  }
+  
+  render(){
+    if (this.state.loading) {
+      return <ActivityIndicator />;
+    }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header>
+        <Title style={styles.cabecario}>Lista</Title>
+      </Header>
+     
+        <List>
+          <ListItem>
+            <Icon name='book' style={styles.icones}/>
+            <Text>Disciplina</Text>
+          </ListItem>
+          <ListItem>
+            <Icon type='FontAwesome' name='globe' style={styles.icones}/>
+            <Text>Normas da ABNT</Text>
+          </ListItem>
+          <ListItem>
+            <Icon type='FontAwesome' name='tasks' style={styles.icones}/>
+            <Text>Repositorio</Text>
+          </ListItem>
+          <ListItem>
+            <Icon type='FontAwesome' name='cubes' style={styles.icones}/>
+            <Text>Ferramentas</Text>
+          </ListItem>
+          <ListItem>
+            <Icon type='FontAwesome' name='mortar-board' style={styles.icones} />
+            <Text>Ferramentas</Text>
+          </ListItem>
+          <ListItem>
+            <Icon type='FontAwesome' name='question' style={styles.icones}/>
+            <Text>Duvidas</Text>
+          </ListItem>
+        </List>
+   
     </View>
   );
+  }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ icones: {
+    fontSize: 20,
+     color: 'red',
+     marginRight: 20
   },
+  cabecario:{
+    alignItems:"center",
+    marginTop:10 
+  }
 });
